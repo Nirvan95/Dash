fcd5ac0908c9b27725535bd49d8d295891320ef7
Nirvan95 <nirvanpandagre65@gmail.com>
2025-02-21 18:04:13 +0530

    actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,6 +7,7 @@ import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
 const FormSchema = z.object({
     id: z.string(),
     customerId: z.string(),
@@ -16,6 +17,8 @@ const FormSchema = z.object({
   });
    
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -34,3 +37,21 @@ export async function createInvoice(formData: FormData) {
     redirect('/dashboard/invoices');
   }
  
+  export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
